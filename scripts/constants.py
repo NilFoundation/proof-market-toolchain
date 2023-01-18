9a2c64a492dcdcc1b7736ffdcba6c9dcd4777c68
hgedia <haresh.gedia@gmail.com>
2023-01-18 11:54:22 +0000

    Updating conflicts with main

--- scripts/constants.py
+++ scripts/constants.py
@@ -2,5 +2,5 @@ import os
 
 VERSION = open(os.path.dirname(os.path.abspath(__file__)) + "/../VERSION", "r").read()
 URL = 'http://try.dbms.nil.foundation/'
-DB_NAME = 'market'
+DB_NAME = 'market2'
 MOUNT = '/v' + VERSION.replace('.', '_')
