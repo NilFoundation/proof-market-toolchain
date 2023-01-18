c53a4288192a2551c81914eafbfc08d4cc89a89b
hgedia <haresh.gedia@gmail.com>
2023-01-18 21:16:54 +0000

    reverting constants

--- scripts/constants.py
+++ scripts/constants.py
@@ -2,5 +2,5 @@ import os
 
 VERSION = open(os.path.dirname(os.path.abspath(__file__)) + "/../VERSION", "r").read()
 URL = 'http://try.dbms.nil.foundation/'
-DB_NAME = 'market2'
+DB_NAME = 'market'
 MOUNT = '/v' + VERSION.replace('.', '_')
