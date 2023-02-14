339f5a46efaf0541c6ede24baf42bc47d4793ffa
vitalylativ <57633422+vitalylativ@users.noreply.github.com>
2023-02-14 20:08:38 +0300

    updated signup script: added producer registration

--- scripts/signup.py
+++ scripts/signup.py
@@ -1,6 +1,7 @@
 import argparse
 from constants import URL, DB_NAME, MOUNT
 import requests
+from auth_tools import get_headers
 
 def signup(args):
 
@@ -17,15 +18,33 @@ def signup(args):
     else:
         print(response.text)
 
+def register_producer(args):
+    headers = get_headers(None)
+    url = URL + f'_db/{DB_NAME}/{MOUNT}/producer/register'
+    body = {
+        'description': 'test producer',
+    }
+
+    response = requests.post(url, json=body, headers=headers)
+    if response.status_code != 200:
+        print(f"Error: {response.status_code} {response.text}")
+    else:
+        print(response.text)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--user', action='store', required=True,
+    subparsers = parser.add_subparsers(help="sub-command help")
+    parser_user = subparsers.add_parser("user", help="user signup")
+    parser_user.set_defaults(func=signup)
+    parser_producer = subparsers.add_parser("producer", help="producer signup")
+    parser_producer.set_defaults(func=register_producer)
+    parser_user.add_argument('-u', '--user', action='store', required=True,
                         help='user name')
-    parser.add_argument('-p', '--passwd', action='store', required=True,
+    parser_user.add_argument('-p', '--passwd', action='store', required=True,
                         help='password')
-    parser.add_argument('-e', '--email', action='store', required=True,
+    parser_user.add_argument('-e', '--email', action='store', required=True,
                         help='email')
     args = parser.parse_args()
 
-    signup(args)
+    args.func(args)
