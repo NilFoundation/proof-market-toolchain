4d4974036195f9daea526021473c67dc710307fa
Gurgen Arakelov <g.g.arakelov@gmail.com>
2023-04-27 23:00:15 +0200

    updated submodules & minor changes in config files

--- scripts/build_scripts/prepare_docker.sh
+++ scripts/build_scripts/prepare_docker.sh
@@ -7,9 +7,7 @@ PROOF_PRODUCER_USER_SECRET='skm'
 echo $PROOF_PRODUCER_USER > .user
 echo $PROOF_PRODUCER_USER_SECRET > .secret
 
-git clone --recurse-submodules https://github.com/NilFoundation/proof-market-toolchain
-cd proof-market-toolchain
-git checkout $PROOF_MARKET_TOOCLHAIN_BRANCH
-cd ..
-docker image  build . -t proof-producer-image
+git clone --recurse-submodules https://github.com/NilFoundation/proof-market-toolchain -b $PROOF_MARKET_TOOCLHAIN_BRANCH
+
+docker image  build . -t proof-producer
 docker run -it proof-producer-image
