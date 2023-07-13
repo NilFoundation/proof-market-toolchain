8c3d7f08e2c5a54796695899063c7270603feb50
Ilya Marozau <dehibeo@gmail.com>
2023-07-13 22:26:40 +0200

    Small bug fix in url. Added Dockerfile for request_pusher. Added docker-compose to manage services (now supports request_pusher service only)

--- scripts/constants.py
+++ scripts/constants.py
@@ -3,6 +3,6 @@ import os
 VERSION = open(os.path.dirname(os.path.abspath(__file__)) + "/../VERSION", "r").read()
 URL = "https://api.proof.market.nil.foundation/"
 DB_NAME = "market"
-MOUNT = "/v" + VERSION.replace(".", "_")
+MOUNT = "v" + VERSION.replace(".", "_")
 AUTH_FILE = "./.auth.json"
 REQUEST_TIMEOUT = 100
